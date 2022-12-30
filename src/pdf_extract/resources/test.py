279bf1da7a3ebdc27e6715386b670ec46223fe83
AVoss <a.vosseler@gmx.net>
2022-12-30 01:05:37 +0100

    Update test.py

--- src/pdf_extract/resources/test.py
+++ src/pdf_extract/resources/test.py
@@ -11,3 +11,10 @@ reload(preproc)
 cleaner = preproc.clean_text(language='english', lemma = False, stem = False)
 
 #X_cl = cleaner.fit_transform(X)
+
+combine_fct = lambda x: '__label__{}'.format(x.label)
+
+train_set['fasttext'] = train_set.apply(combine_fct, axis = 1)
+
+X = train_set[['fasttext', 'text']].reset_index(drop=True)
+X.head()
